41c6436f6b6b9acd592cf06c4266368bb5b356ce
Jeremy Brown <jj.brown@hotmail.com>
2014-01-31 13:50:17 -0500

    Added desktop project to repository

---
+++ ss-java-desktop/src/com/asdf/ssjava/Main.java
@@ -0,0 +1,16 @@
+package com.asdf.ssjava;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class Main {
+	public static void main(String[] args) {
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "ss-java";
+		cfg.useGL20 = false;
+		cfg.width = 480;
+		cfg.height = 320;
+		
+		new LwjglApplication(new SSJava(), cfg);
+	}
+}
