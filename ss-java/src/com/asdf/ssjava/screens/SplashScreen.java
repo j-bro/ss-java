a3469aa362ecb007083061643a052f2bd439b8fa
Jeremy Brown <jj.brown@hotmail.com>
2014-02-07 09:16:53 -0500

    First fade-in splash screen
    *note: Tween animation causes crash on Android

---
+++ ss-java/src/com/asdf/ssjava/screens/SplashScreen.java
@@ -0,0 +1,84 @@
+package com.asdf.ssjava.screens;
+
+import aurelienribon.tweenengine.Tween;
+import aurelienribon.tweenengine.TweenEquations;
+import aurelienribon.tweenengine.TweenManager;
+
+import com.asdf.ssjava.SSJava;
+import com.asdf.ssjava.tweenaccessors.SpriteTween;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL10;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.Texture.TextureFilter;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class SplashScreen implements Screen {
+
+	Texture splashTexture;
+	Sprite splashSprite;
+	SpriteBatch batch;
+	SSJava game;
+	TweenManager manager;
+	
+	public SplashScreen(SSJava game) {
+		this.game = game;
+	}
+	
+	@Override
+	public void render(float delta) {
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+		
+		manager.update(delta);
+		
+		batch.begin();
+			splashSprite.draw(batch);
+		batch.end();
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		
+	}
+
+	@Override
+	public void show() {
+		splashTexture = new Texture("data/SplashScreen.png");
+		splashTexture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
+		
+		splashSprite = new Sprite(splashTexture);
+		splashSprite.setPosition(Gdx.graphics.getWidth() / 2 - splashSprite.getWidth() / 2, Gdx.graphics.getHeight() / 2 - splashSprite.getHeight() / 2);
+		splashSprite.setColor(1, 1, 1, 0);
+		
+		batch = new SpriteBatch();
+		
+		Tween.registerAccessor(Sprite.class, new SpriteTween());
+		
+		manager = new TweenManager();
+		
+		Tween.to(splashSprite, SpriteTween.ALPHA, 3f).target(1).ease(TweenEquations.easeInQuad).start(manager);
+	}
+
+	@Override
+	public void hide() {
+		
+	}
+
+	@Override
+	public void pause() {
+		
+	}
+
+	@Override
+	public void resume() {
+		
+	}
+
+	@Override
+	public void dispose() {
+		
+	}
+
+}
