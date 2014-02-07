85b46c014e06dc02cd473c38d98976e24b884439
Jeremy Brown <jj.brown@hotmail.com>
2014-02-06 19:21:22 -0500

    [DESKTOP] Set window size to 1080x720

--- ss-java-desktop/src/com/asdf/ssjava/Main.java
+++ ss-java-desktop/src/com/asdf/ssjava/Main.java
@@ -3,13 +3,14 @@ package com.asdf.ssjava;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
+
 public class Main {
 	public static void main(String[] args) {
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
 		cfg.title = "ss-java";
 		cfg.useGL20 = false;
-		cfg.width = 480;
-		cfg.height = 320;
+		cfg.width = 1080;
+		cfg.height = 720;
 		
 		new LwjglApplication(new SSJava(), cfg);
 	}
