0d17a1ceb8275741c921fec28cc61f103fa8fc37
Jeremy Brown <jj.brown@hotmail.com>
2014-02-25 16:02:35 -0500

    Added power up skeleton class

---
+++ ss-java/src/com/asdf/ssjava/entities/Powerup.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package com.asdf.ssjava.entities;
+
+import com.badlogic.gdx.math.Vector2;
+
+/**
+ * @author Jeremy Brown
+ * @author Simon Thompson
+ * 
+ */
+public class Powerup extends MoveableEntity {
+
+	/**
+	 * 
+	 */
+	public Powerup(Vector2 position, float width, float height, float SPEED, float rotation) {
+		super(position, width, height, SPEED, rotation);
+	}
+
+	@Override
+	public void advance(float delta) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
