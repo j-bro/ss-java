10a5266901424e53375ee22191bfb2b6cc3c15f8
Jeremy Brown <jj.brown@hotmail.com>
2014-03-03 16:12:12 -0500

    Implemented mouse and touch movement and ship firing. Resulting acceleration set to DEFAULT_ACCELERATION field in Ship type.

--- ss-java/src/com/asdf/ssjava/world/InputManager.java
+++ ss-java/src/com/asdf/ssjava/world/InputManager.java
@@ -6,6 +6,7 @@ package com.asdf.ssjava.world;
 import com.asdf.ssjava.SSJava;
 import com.asdf.ssjava.entities.Ship;
 import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputProcessor;
 
 /**
@@ -47,16 +48,16 @@ public class InputManager implements InputProcessor {
 		// TODO add keyconfigs 
 		switch (keycode) {
 			case Keys.UP: 
-				ship.getAcceleration().y = 3;
+				ship.getAcceleration().y = ship.DEFAULT_ACCELERATION.y;
 				break;
 			case Keys.DOWN: 
-				ship.getAcceleration().y = -3;
+				ship.getAcceleration().y = (-1) *  ship.DEFAULT_ACCELERATION.y;
 				break;
 			case Keys.W:
-				ship.getAcceleration().y = 3;
+				ship.getAcceleration().y = ship.DEFAULT_ACCELERATION.y;
 				break;
 			case Keys.S:
-				ship.getAcceleration().y = -3;
+				ship.getAcceleration().y = (-1) * ship.DEFAULT_ACCELERATION.y;
 				break;
 			case Keys.SPACE: 
 				ship.fire();
@@ -88,11 +89,8 @@ public class InputManager implements InputProcessor {
 		case Keys.SPACE: 
 			
 			break;
-		case Keys.ENTER: // temp
-			
-			break;
 		default: break;
-	}
+		}
 	return true;
 	}
 
@@ -110,8 +108,20 @@ public class InputManager implements InputProcessor {
 	 */
 	@Override
 	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-		// TODO Auto-generated method stub
-		return false;
+		// Ship movement input
+		if (screenX < Gdx.graphics.getWidth() / 2) {
+			if (screenY > Gdx.graphics.getHeight() / 2) {
+				ship.getAcceleration().y = (-1) * ship.DEFAULT_ACCELERATION.y;
+			}
+			else {
+				ship.getAcceleration().y = ship.DEFAULT_ACCELERATION.y;
+			}
+		}
+		// Ship firing input
+		else {
+			ship.fire();
+		}
+		return true;
 	}
 
 	/* (non-Javadoc)
@@ -120,7 +130,13 @@ public class InputManager implements InputProcessor {
 	@Override
 	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
 		// TODO Auto-generated method stub
-		return false;
+		if (screenY > Gdx.graphics.getHeight() / 2) {
+			ship.getAcceleration().y = 0;
+		}
+		else {
+			ship.getAcceleration().y = 0;
+		}
+		return true;
 	}
 
 	/* (non-Javadoc)
