22d346fde53da12a44284dd302e69e3c0df1da8b
Jeremy Brown <jj.brown@hotmail.com>
2014-03-03 16:09:10 -0500

    Added bullets and bullet (strip) texture

---
+++ ss-java/src/com/asdf/ssjava/entities/Bullet.java
@@ -0,0 +1,43 @@
+/**
+ * Entity model for a bullet.
+ * Adds starting velocity which can be in any direction, but will most likely be in that of the ship's velocity OR only horizontal.
+ */
+package com.asdf.ssjava.entities;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.math.Vector2;
+
+/**
+ * @author Jeremy Brown
+ * @author Simon Thompson
+ * 
+ */
+public class Bullet extends MoveableEntity {
+	
+	/**
+	 * The bullet's default velocity
+	 */
+	public final Vector2 DEFAULT_VELOCITY = new Vector2(0, 15);
+
+	/**
+	 * @param position the position of the bullet
+	 * @param width the width of the bullet
+	 * @param height the height of the bullet
+	 * @param rotation the rotation of the bullet in degrees
+	 */
+	public Bullet(Vector2 position, float width, float height, float rotation, Vector2 velocity) {
+		super(position, width, height, rotation); 
+		this.velocity = velocity;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.asdf.ssjava.entities.MoveableEntity#update()
+	 */
+	@Override
+	public void update() {
+		position.add(velocity.cpy().scl(Gdx.graphics.getDeltaTime())); 
+		super.update();
+	}
+	
+	
+}
