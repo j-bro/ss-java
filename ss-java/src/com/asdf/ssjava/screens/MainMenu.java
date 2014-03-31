ce90c88609ea60164a5583ab938c6209bb327cf0
Jeremy Brown <jj.brown@hotmail.com>
2014-03-31 18:28:16 -0400

    Added copyright to main menu

--- ss-java/src/com/asdf/ssjava/screens/MainMenu.java
+++ ss-java/src/com/asdf/ssjava/screens/MainMenu.java
@@ -23,7 +23,6 @@ import com.badlogic.gdx.scenes.scene2d.utils.Align;
  * @author Jeremy Brown
  *
  */
-
 public class MainMenu implements Screen {
 	
 	SSJava game;
@@ -32,6 +31,7 @@ public class MainMenu implements Screen {
 	BitmapFont whiteFont;
 	
 	Label titleLabel;
+	Label creditsLabel;
 	
 	TextButton playButton;
 	MenuButton optionsButton;
@@ -49,6 +49,10 @@ public class MainMenu implements Screen {
 		this.game = game;
 	}
 	
+	/*
+	 * (non-Javadoc)
+	 * @see com.badlogic.gdx.Screen#render(float)
+	 */
 	@Override
 	public void render(float delta) {
 		Gdx.gl.glClearColor(0, 0, 0, 1);
@@ -61,6 +65,10 @@ public class MainMenu implements Screen {
 		batch.end();
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * @see com.badlogic.gdx.Screen#resize(int, int)
+	 */
 	@Override
 	public void resize(int width, int height) {
 		if (stage == null) {
@@ -76,11 +84,19 @@ public class MainMenu implements Screen {
 		playButton.setY(Gdx.graphics.getHeight() / 2 - playButton.getHeight() / 2 + 140);
 		
 		playButton.addListener(new InputListener() {
+			/*
+			 * (non-Javadoc)
+			 * @see com.badlogic.gdx.scenes.scene2d.InputListener#touchDown(com.badlogic.gdx.scenes.scene2d.InputEvent, float, float, int, int)
+			 */
 			public boolean touchDown(InputEvent even, float x, float y, int pointer, int button) {
 				Gdx.app.log(SSJava.LOG, "Play button down");
 				return true;
 			}
 			
+			/*
+			 * (non-Javadoc)
+			 * @see com.badlogic.gdx.scenes.scene2d.InputListener#touchUp(com.badlogic.gdx.scenes.scene2d.InputEvent, float, float, int, int)
+			 */
 			public void touchUp(InputEvent even, float x, float y, int pointer, int button) {
 				Gdx.app.log(SSJava.LOG, "Play button up");
 				game.setScreen(new LevelSelectMenu(game, thisMainMenu));
@@ -94,11 +110,19 @@ public class MainMenu implements Screen {
 		optionsButton.setY(Gdx.graphics.getHeight() / 2 - optionsButton.getHeight() / 2 + 60);
 		
 		optionsButton.addListener(new InputListener() {
+			/*
+			 * (non-Javadoc)
+			 * @see com.badlogic.gdx.scenes.scene2d.InputListener#touchDown(com.badlogic.gdx.scenes.scene2d.InputEvent, float, float, int, int)
+			 */
 			public boolean touchDown(InputEvent even, float x, float y, int pointer, int button) {
 				Gdx.app.log(SSJava.LOG, "Options button down");
 				return true;
 			}
 			
+			/*
+			 * (non-Javadoc)
+			 * @see com.badlogic.gdx.scenes.scene2d.InputListener#touchUp(com.badlogic.gdx.scenes.scene2d.InputEvent, float, float, int, int)
+			 */
 			public void touchUp(InputEvent even, float x, float y, int pointer, int button) {
 				Gdx.app.log(SSJava.LOG, "Options button up");
 				game.setScreen(new OptionsMenu(game, thisMainMenu));
@@ -111,11 +135,19 @@ public class MainMenu implements Screen {
 		highScoresButton.setY(Gdx.graphics.getHeight() / 2 - highScoresButton.getHeight() / 2 - 20);
 		
 		highScoresButton.addListener(new InputListener() {
+			/*
+			 * (non-Javadoc)
+			 * @see com.badlogic.gdx.scenes.scene2d.InputListener#touchDown(com.badlogic.gdx.scenes.scene2d.InputEvent, float, float, int, int)
+			 */
 			public boolean touchDown(InputEvent even, float x, float y, int pointer, int button) {
 				Gdx.app.log(SSJava.LOG, "High scores button down");
 				return true;
 			}
 			
+			/*
+			 * (non-Javadoc)
+			 * @see com.badlogic.gdx.scenes.scene2d.InputListener#touchUp(com.badlogic.gdx.scenes.scene2d.InputEvent, float, float, int, int)
+			 */
 			public void touchUp(InputEvent even, float x, float y, int pointer, int button) {
 				Gdx.app.log(SSJava.LOG, "High scores button up");
 				game.setScreen(new HighScoresMenu(game, thisMainMenu));
@@ -128,11 +160,19 @@ public class MainMenu implements Screen {
 		creditsButton.setY(Gdx.graphics.getHeight() / 2 - creditsButton.getHeight() / 2 - 100);
 		
 		creditsButton.addListener(new InputListener() {
+			/*
+			 * (non-Javadoc)
+			 * @see com.badlogic.gdx.scenes.scene2d.InputListener#touchDown(com.badlogic.gdx.scenes.scene2d.InputEvent, float, float, int, int)
+			 */
 			public boolean touchDown(InputEvent even, float x, float y, int pointer, int button) {
 				Gdx.app.log(SSJava.LOG, "Credits button down");
 				return true;
 			}
 			
+			/*
+			 * (non-Javadoc)
+			 * @see com.badlogic.gdx.scenes.scene2d.InputListener#touchUp(com.badlogic.gdx.scenes.scene2d.InputEvent, float, float, int, int)
+			 */
 			public void touchUp(InputEvent even, float x, float y, int pointer, int button) {
 				Gdx.app.log(SSJava.LOG, "Credits button up");
 				game.setScreen(new CreditsMenu(game, thisMainMenu));
@@ -145,11 +185,19 @@ public class MainMenu implements Screen {
 		exitButton.setY(Gdx.graphics.getHeight() / 2 - exitButton.getHeight() / 2 - 180);
 		
 		exitButton.addListener(new InputListener() {
+			/*
+			 * (non-Javadoc)
+			 * @see com.badlogic.gdx.scenes.scene2d.InputListener#touchDown(com.badlogic.gdx.scenes.scene2d.InputEvent, float, float, int, int)
+			 */
 			public boolean touchDown(InputEvent even, float x, float y, int pointer, int button) {
 				Gdx.app.log(SSJava.LOG, "Exit button down");
 				return true;
 			}
 			
+			/*
+			 * (non-Javadoc)
+			 * @see com.badlogic.gdx.scenes.scene2d.InputListener#touchUp(com.badlogic.gdx.scenes.scene2d.InputEvent, float, float, int, int)
+			 */
 			public void touchUp(InputEvent even, float x, float y, int pointer, int button) {
 				Gdx.app.log(SSJava.LOG, "Exit button up");
 				Gdx.app.exit();
@@ -165,7 +213,13 @@ public class MainMenu implements Screen {
 		titleLabel.setWidth(width);
 		titleLabel.setAlignment(Align.center);
 		
-		stage.addActor(titleLabel);		
+		// bottom right credits text
+		creditsLabel = new Label("(C) 2014 ASDF", ls);
+		creditsLabel.setX(width - creditsLabel.getWidth() - 10);
+		creditsLabel.setY(10);
+		
+		stage.addActor(titleLabel);	
+		stage.addActor(creditsLabel);
 		stage.addActor(playButton);
 		stage.addActor(optionsButton);
 		stage.addActor(highScoresButton);
@@ -173,6 +227,10 @@ public class MainMenu implements Screen {
 		stage.addActor(exitButton);
 	}
  
+	/*
+	 * (non-Javadoc)
+	 * @see com.badlogic.gdx.Screen#show()
+	 */
 	@Override
 	public void show() {
 		Gdx.app.log(SSJava.LOG, "Show main menu");
@@ -181,21 +239,37 @@ public class MainMenu implements Screen {
 		whiteFont = game.assetManager.get("data/fonts/whitefont.fnt", BitmapFont.class);		
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * @see com.badlogic.gdx.Screen#hide()
+	 */
 	@Override 
 	public void hide() {
 		dispose();
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * @see com.badlogic.gdx.Screen#pause()
+	 */
 	@Override
 	public void pause() {
 
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * @see com.badlogic.gdx.Screen#resume()
+	 */
 	@Override
 	public void resume() {
 
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * @see com.badlogic.gdx.Screen#dispose()
+	 */
 	@Override
 	public void dispose() {
 		batch.dispose();
