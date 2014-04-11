6c05c0d7e69beabe8e7912969fc9b185c980bb1e
Simon Thompson <emperorsmiley@hotmail.com>
2014-04-11 10:13:47 -0500

    sun

--- ss-java/src/com/asdf/ssjava/entities/Sun.java
+++ ss-java/src/com/asdf/ssjava/entities/Sun.java
@@ -1,13 +1,43 @@
 package com.asdf.ssjava.entities;
 
-public class Sun {
+import com.badlogic.gdx.math.Vector2;
+
+public class Sun extends Obstacle{
 
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	/**
+	 * The planet's default velocity (static)
+	 */
+	public static final Vector2 DEFAULT_VELOCITY = new Vector2(0, 0);
+	
+	/**
+	 * 
+	 * @param position
+	 * @param width
+	 * @param height
+	 * @param rotation
+	 */
+	public Sun(Vector2 position, float width, float height, float rotation) {
+		super(position, width, height, rotation);
+	}
 
+	/*
+	 * (non-Javadoc)
+	 * @see com.asdf.ssjava.entities.Obstacle#getDEFAULT_VELOCITY()
+	 */
+	@Override
+	public Vector2 getDEFAULT_VELOCITY() {
+		return DEFAULT_VELOCITY;
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	public String toString() {
+		return "Sun";
 	}
 
 }
