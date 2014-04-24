4533e0600e3b9557531636a168f3dcb90c305789
Jeremy Brown <jj.brown@hotmail.com>
2014-04-24 14:09:12 -0400

    Placeholders for power up sounds in AudioPlayer class

--- ss-java/src/com/asdf/ssjava/AudioPlayer.java
+++ ss-java/src/com/asdf/ssjava/AudioPlayer.java
@@ -18,10 +18,16 @@ public class AudioPlayer {
 
 	public static Music gameMusic = Gdx.audio.newMusic(Gdx.files.internal("data/music/ambience.mp3"));
 	
-	public static Sound shot = Gdx.audio.newSound(Gdx.files.internal("data/sounds/pewpew.mp3"));
-	public static Sound bulletImpact = Gdx.audio.newSound(Gdx.files.internal("data/sounds/explosion.mp3"));
-	public static Sound shipImpact = Gdx.audio.newSound(Gdx.files.internal("data/sounds/explosion.mp3"));
+	public static Sound shotSound = Gdx.audio.newSound(Gdx.files.internal("data/sounds/pewpew.mp3"));
+	public static Sound bulletImpactSound = Gdx.audio.newSound(Gdx.files.internal("data/sounds/explosion.mp3"));
+	public static Sound shipImpactSound = Gdx.audio.newSound(Gdx.files.internal("data/sounds/explosion.mp3"));
+//	public static Sound healthUpSound = Gdx.audio.newSound(Gdx.files.internal(""));
+//	public static Sound speedOfLightSound = Gdx.audio.newSound(Gdx.files.internal(""));
 	
+	/**
+	 * 
+	 * @param looping whether or not the game is to loop
+	 */
 	public static void playGameMusic(boolean looping) {
 		
 		gameMusic.setVolume(SSJava.prefs.getInteger("musicVolume", 100) / 100f);
@@ -29,24 +35,33 @@ public class AudioPlayer {
 		gameMusic.play();
 	}
 	
+	/**
+	 * Pauses the game music
+	 */
 	public static void pauseGameMusic() {
 		gameMusic.pause();
 	}
 	
+	/**
+	 * Stops the game music
+	 */
 	public static void stopGameMusic() {
 		gameMusic.stop();
 	}
+	
 	/**
 	 * Plays the shot sound for a fired bullet
 	 */
 	public static void shoot() {
 		new Thread(new Runnable() {
-
+			/*
+			 * (non-Javadoc)
+			 * @see java.lang.Runnable#run()
+			 */
 			@Override
 			public void run() {
-				shot.play(SSJava.prefs.getInteger("soundVolume", 100) / 100f);
+				shotSound.play(SSJava.prefs.getInteger("soundVolume", 100) / 100f);
 			}
-			
 		}).start();
 	}
 	
@@ -55,10 +70,13 @@ public class AudioPlayer {
 	 */
 	public static void bulletImpact() {
 		new Thread(new Runnable() {
-
+			/*
+			 * (non-Javadoc)
+			 * @see java.lang.Runnable#run()
+			 */
 			@Override
 			public void run() {
-				bulletImpact.play(SSJava.prefs.getInteger("soundVolume", 100) / 100f);
+				bulletImpactSound.play(SSJava.prefs.getInteger("soundVolume", 100) / 100f);
 			}
 		}).start();
 	}
@@ -68,18 +86,55 @@ public class AudioPlayer {
 	 */
 	public static void shipImpact() {
 		new Thread(new Runnable() {
-
+			/*
+			 * (non-Javadoc)
+			 * @see java.lang.Runnable#run()
+			 */
 			@Override
 			public void run() {
-				shipImpact.play(SSJava.prefs.getInteger("soundVolume", 100) / 100f);
+//				shipImpactSound.play(SSJava.prefs.getInteger("soundVolume", 100) / 100f);
+			}
+		}).start();
+	}
+	
+	/**
+	 * Plays the explosion sound for the ship impact
+	 */
+	public static void healthUp() {
+		new Thread(new Runnable() {
+			/*
+			 * (non-Javadoc)
+			 * @see java.lang.Runnable#run()
+			 */
+			@Override
+			public void run() {
+//				healthUpSound.play(SSJava.prefs.getInteger("soundVolume", 100) / 100f);
+			}
+		}).start();
+	}
+	
+	/**
+	 * Plays the explosion sound for the ship impact
+	 */
+	public static void speedOfLight() {
+		new Thread(new Runnable() {
+			/*
+			 * (non-Javadoc)
+			 * @see java.lang.Runnable#run()
+			 */
+			@Override
+			public void run() {
+//				speedOfLightSound.play(SSJava.prefs.getInteger("soundVolume", 100) / 100f);
 			}
 		}).start();
 	}
 	
 	public static void dispose() {
 		gameMusic.dispose();
-		shot.dispose();
-		bulletImpact.dispose();
-		shipImpact.dispose();
+		shotSound.dispose();
+		bulletImpactSound.dispose();
+		shipImpactSound.dispose();
+//		healthUp.dispose();
+//		speedOfLight.dispose();
 	}
 }
