521c7476ae30a0233d79bb45ce1ecad5e358b573
Jeremy Brown <jj.brown@hotmail.com>
2014-05-01 16:49:54 -0400

    Misc fixes

--- ss-java-desktop/src/com/asdf/ssjava/Main.java
+++ ss-java-desktop/src/com/asdf/ssjava/Main.java
@@ -7,7 +7,7 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 public class Main {
 	public static void main(String[] args) {
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
-		cfg.title = "ss-java";
+		cfg.title = "SS-Java";
 		cfg.useGL20 = false;
 		cfg.width = 1280;
 		cfg.height = 720;
